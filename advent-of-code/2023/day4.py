738fbb338b31eb7f834768b309445cbd74834e9c
vimturtle <arcsine@tutanota.com>
2023-12-05 19:35:45 +0545

    fixes

--- advent-of-code/2023/day4.py
+++ advent-of-code/2023/day4.py
@@ -5,14 +5,13 @@ p1 = 0
 p2 = {k + 1: 1 for k in range(len(cards))}
 
 for cardid, card in enumerate(cards, 1):
-    nums = card.split(": ")[1].split(" | ")
-    num_common = len([x for x in nums[0].split() if x in nums[1].split()])
+    win, have = map(str.split, card.split(": ")[1].split(" | "))
+    num_common = len(set(win) & set(have))
 
     if num_common:
         p1 += 2 ** (num_common - 1)
 
-        for i in range(p2[cardid]):
-            for j in range(num_common):
-                p2[cardid + j + 1] += 1
+        for i in range(num_common):
+            p2[cardid + i + 1] += p2[cardid]
 
 print(p1, sum(p2.values()))
---
+++ advent-of-code/2023/day5.py
@@ -0,0 +1,4 @@
+with open("input/day5.txt", "r") as f:
+    maps = f.read()
+
+print(maps)
