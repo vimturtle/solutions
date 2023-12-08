1cd7dbac52583d6fc73f1f56b4b61a1ebb9c9ca5
vimturtle <arcsine@tutanota.com>
2023-12-08 22:52:01 +0545

    fixes

--- advent-of-code/2023/day8.py
+++ advent-of-code/2023/day8.py
@@ -1,15 +1,10 @@
 import math
 
+
 with open("input/day8.txt", "r") as f:
     info = f.read().splitlines()
     direcs = info[0]
-    coords = info[2:]
-
-    coord_dict = {}
-    for coord in coords:
-        k = coord[:3]
-        v = (coord[7:10], coord[12:15])
-        coord_dict[k] = v
+    coords = {coord[:3]: (coord[7:10], coord[12:15]) for coord in info[2:]}
 
 
 def follow(start, is_ghost=False):
@@ -17,7 +12,7 @@ def follow(start, is_ghost=False):
     head = start
 
     while (not head.endswith("Z")) if is_ghost else (head != "ZZZ"):
-        head = coord_dict[head][0 if direcs[steps % len(direcs)] == "L" else 1]
+        head = coords[head][0 if direcs[steps % len(direcs)] == "L" else 1]
         steps += 1
 
     return steps
@@ -25,7 +20,7 @@ def follow(start, is_ghost=False):
 
 p1 = follow("AAA")
 p2 = math.lcm(
-    *[follow(head, is_ghost=True) for head in coord_dict.keys() if head.endswith("A")]
+    *[follow(head, is_ghost=True) for head in coords.keys() if head.endswith("A")]
 )
 
 print(p1, p2)
