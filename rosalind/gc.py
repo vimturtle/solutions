fcc02266c0e26772823bedf0a1084a69e0f3422b
vimturtle <arcsine@tutanota.com>
2023-12-10 23:15:48 +0545

    fixes

--- rosalind/gc.py
+++ rosalind/gc.py
@@ -6,18 +6,17 @@ curr_id = None
 
 for line in lines:
     if line.startswith(">"):
-        strings[line] = ""
-        curr_id = line
+        curr_id = line[1:]
+        strings[curr_id] = ""
     else:
         strings[curr_id] += line
 
-high_gc = 0
-high_id = ""
+max_gc_id, max_gc = "", 0
 
 for k, v in strings.items():
     gc = len([c for c in v if c in ["C", "G"]]) * 100 / len(v)
-    if gc > high_gc:
-        high_gc = gc
-        high_id = k
+    if gc > max_gc:
+        max_gc_id = k
+        max_gc = gc
 
-print(high_id[1:] + "\n" + str(round(high_gc, 6)))
+print(max_gc_id + "\n" + str(round(max_gc, 6)))
