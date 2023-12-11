04c5ac457b2c28d6390a6f439a0903b39b69695d
vimturtle <arcsine@tutanota.com>
2023-12-11 13:10:26 +0545

    fixes

--- advent-of-code/2023/day9.py
+++ advent-of-code/2023/day9.py
@@ -2,9 +2,9 @@ with open("input/day9.txt", "r") as f:
     lines = [[int(x) for x in line.split()] for line in f.read().splitlines()]
 
 
-def extrapolate(l):
-    diffs = [b - a for a, b in zip(l, l[1:])]
-    return l[-1] + extrapolate(diffs) if l else 0
+def extrapolate(line):
+    diffs = [b - a for a, b in zip(line, line[1:])]
+    return line[-1] + extrapolate(diffs) if line else 0
 
 
 print(sum(extrapolate(line) for line in lines))
